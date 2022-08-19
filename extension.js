92fa9afbc82f8f084c2069ea65b4261da0edeaaf
Mark Lavercombe <mlava@users.noreply.github.com>
2022-08-19 19:16:50 +1000

    Add files via upload

--- extension.js
+++ extension.js
@@ -7,12 +7,23 @@ const config = {
             description: "Banner height in pixels",
             action: { type: "input", placeholder: "150" },
         },
+        {
+            id: "bh-gradient",
+            name: "Gradient to bottom",
+            description: "Apply gradient top to bottom",
+            action: { type: "switch" },
+        },
     ]
 };
 
 export default {
     onload: ({ extensionAPI }) => {
         extensionAPI.settings.panel.create(config);
+
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "Set Banner from Clipboard",
+            callback: () => setBannerClip({ extensionAPI })
+        });
 
         window.addEventListener('hashchange', async function (e) {
             checkBanner({ extensionAPI });
@@ -22,6 +33,9 @@ export default {
         if (document.querySelector("div.bannerDiv")) {
             document.querySelector("div.bannerDiv").remove();
         }
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Set Banner from Clipboard'
+        });
     }
 }
 
@@ -33,6 +47,7 @@ function sendConfigAlert(key) {
 
 async function checkBanner({ extensionAPI }) {
     var bannerHeight, key;
+    var bannerGradient = false;
     breakme: {
         if (extensionAPI.settings.get("bh-height")) {
             const regex = /^\d{2,3}$/;
@@ -45,6 +60,9 @@ async function checkBanner({ extensionAPI }) {
             }
         } else {
             bannerHeight = "150";
+        }
+        if (extensionAPI.settings.get("bh-gradient") == true) {
+            bannerGradient = true;
         }
     }
 
@@ -61,17 +79,80 @@ async function checkBanner({ extensionAPI }) {
                 bannerURL = info[0][0].children[i].string;
                 bannerURL1 = bannerURL.split("banner: ")
                 finalURL = bannerURL1[1];
-                setBanner(finalURL, bannerHeight);
+                setBanner(finalURL, bannerHeight, bannerGradient);
             }
         }
     }
 }
 
-async function setBanner(finalURL, bannerHeight) {
+async function setBanner(finalURL, bannerHeight, bannerGradient) {
     var bannerDiv = document.createElement('div');
     bannerDiv.classList.add('bannerDiv');
     bannerDiv.innerHTML = "";
-    bannerDiv.style.cssText = 'background: url(' + finalURL + ') no-repeat center center; height: ' + bannerHeight + 'px;';
+    if (bannerGradient == true) {
+        bannerDiv.style.cssText = 'background: linear-gradient(to bottom, transparent, white 150%), url(' + finalURL + ') no-repeat center center; height: ' + bannerHeight + 'px;';
+    } else {
+        bannerDiv.style.cssText = 'background: url(' + finalURL + ') no-repeat center center; height: ' + bannerHeight + 'px;';
+    }    
     var roamBody = document.querySelector("#app > div > div > div.flex-h-box > div.roam-main > div.roam-body-main > div > div")
     roamBody.parentNode.insertBefore(bannerDiv, roamBody);
+}
+
+async function setBannerClip({ extensionAPI }) {
+    var bannerHeight, key;
+    var bannerGradient = false;
+    breakme: {
+        if (extensionAPI.settings.get("bh-height")) {
+            const regex = /^\d{2,3}$/;
+            if (extensionAPI.settings.get("bh-height").match(regex)) {
+                bannerHeight = extensionAPI.settings.get("bh-height");
+            } else {
+                key = "height";
+                sendConfigAlert(key);
+                break breakme;
+            }
+        } else {
+            bannerHeight = "150";
+        }
+        if (extensionAPI.settings.get("bh-gradient") == true) {
+            bannerGradient = true;
+        }
+    }
+    const clipText = await navigator.clipboard.readText();
+    if (!isUrl(clipText)) {
+        alert('Please make sure that the clipboard contains a url to an image');
+    } else {
+        var bannerText = "banner: " + clipText;
+        var startBlock = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
+        let q = `[:find (pull ?page [:node/title :block/string :block/uid {:block/children ...} ]) :where [?page :block/uid "${startBlock}"]  ]`;
+        var info = await window.roamAlphaAPI.q(q);
+        if (info.length > 0) {
+            var bannerExists = false;
+            for (var i = 0; i < info[0][0]?.children.length; i++) {
+                if (info[0][0].children[i].string.match("banner: ")) { // there's already a banner
+                    console.log("Updating page banner");
+                    await window.roamAlphaAPI.updateBlock(
+                        { block: { uid: info[0][0].children[i].uid, string: bannerText.toString(), open: true } });
+                    bannerExists = true;
+                }
+            }            
+            if (bannerExists == false) { // no banners found, create one from clipboard
+                console.log("Creating page banner");
+                var thisBlock = window.roamAlphaAPI.util.generateUID();
+                await window.roamAlphaAPI.createBlock({
+                    location: { "parent-uid": info[0][0].uid, order: 10000 },
+                    block: { string: bannerText.toString(), uid: thisBlock }
+                });
+            }
+        }
+        if (document.querySelector("div.bannerDiv")) {
+            document.querySelector("div.bannerDiv").remove();
+        }
+        setBanner(clipText, bannerHeight, bannerGradient);
+    }
+}
+
+function isUrl(s) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return regexp.test(s);
 }
