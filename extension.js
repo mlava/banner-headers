046e0b9f61ce30952f79d30d444c20b6a9a23bc2
Mark Lavercombe <mlava@users.noreply.github.com>
2022-08-20 23:22:00 +1000

    Add files via upload

--- extension.js
+++ extension.js
@@ -41,7 +41,7 @@ export default {
 
 function sendConfigAlert(key) {
     if (key == "height") {
-        alert("Please set your preferred banner height in the configuration settings via the Roam Depot tab.");
+        alert("Please set your preferred banner height in pixels in the configuration settings via the Roam Depot tab.");
     }
 }
 
@@ -64,41 +64,59 @@ async function checkBanner({ extensionAPI }) {
         if (extensionAPI.settings.get("bh-gradient") == true) {
             bannerGradient = true;
         }
-    }
 
-    if (document.querySelector("div.bannerDiv")) {
-        document.querySelector("div.bannerDiv").remove();
-    }
-    var startBlock = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
-    var bannerURL, bannerURL1, finalURL;
-    let q = `[:find (pull ?page [:node/title :block/string {:block/children ...} ]) :where [?page :block/uid "${startBlock}"]  ]`;
-    var info = await window.roamAlphaAPI.q(q);
-    if (info.length > 0) {
-        for (var i = 0; i < info[0][0]?.children.length; i++) {
-            if (info[0][0].children[i].string.match("banner: ")) {
-                bannerURL = info[0][0].children[i].string;
-                bannerURL1 = bannerURL.split("banner: ")
-                finalURL = bannerURL1[1];
-                setBanner(finalURL, bannerHeight, bannerGradient);
+        if (document.querySelector("div.bannerDiv")) {
+            document.querySelector("div.bannerDiv").remove();
+        }
+        var DNP = false;
+        var startBlock = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
+        if (!startBlock) {
+            var uri = window.location.href;
+            const regex = /^https:\/\/roamresearch.com\/#\/(app|offline)\/\w+$/; //today's DNP
+            if (uri.match(regex)) { // this is Daily Notes for today
+                var today = new Date();
+                var dd = String(today.getDate()).padStart(2, '0');
+                var mm = String(today.getMonth() + 1).padStart(2, '0');
+                var yyyy = today.getFullYear();
+                startBlock = mm + '-' + dd + '-' + yyyy;
+                DNP = true;
+            }
+        }
+        let q = `[:find (pull ?page [:node/title :block/string {:block/children ...} ]) :where [?page :block/uid "${startBlock}"]  ]`;
+        var info = await window.roamAlphaAPI.q(q);
+        var bannerURL, bannerURL1, finalURL;
+        if (info.length > 0) {
+            for (var i = 0; i < info[0][0]?.children.length; i++) {
+                if (info[0][0].children[i].string.match("banner: ")) {
+                    bannerURL = info[0][0].children[i].string;
+                    bannerURL1 = bannerURL.split("banner: ")
+                    finalURL = bannerURL1[1];
+                    setBanner(finalURL, bannerHeight, bannerGradient, DNP);
+                }
             }
         }
     }
 }
 
-async function setBanner(finalURL, bannerHeight, bannerGradient) {
+async function setBanner(finalURL, bannerHeight, bannerGradient, DNP) {
     await sleep(50);
     var bannerDiv = document.createElement('div');
     bannerDiv.classList.add('bannerDiv');
     bannerDiv.innerHTML = "";
     var w = getComputedStyle(document.querySelector('#app')).getPropertyValue('width');
-    console.error(w);
+
     bannerDiv.style.minWidth = w.toString();
+    var roamBody;
     if (bannerGradient == true) {
-        bannerDiv.style.cssText = 'background: linear-gradient(to bottom, transparent, white 150%), url(' + finalURL + ') no-repeat center center; height: ' + bannerHeight + 'px; min-width: '+w.toString()+'; ';
+        bannerDiv.style.cssText = 'background: linear-gradient(to bottom, transparent, white 150%), url(' + finalURL + ') no-repeat center center; height: ' + bannerHeight + 'px; min-width: ' + w.toString() + '; ';
     } else {
-        bannerDiv.style.cssText = 'background: url(' + finalURL + ') no-repeat center center; height: ' + bannerHeight + 'px; min-width: '+w.toString()+';';
+        bannerDiv.style.cssText = 'background: url(' + finalURL + ') no-repeat center center; height: ' + bannerHeight + 'px; min-width: ' + w.toString() + ';';
     }
-    var roamBody = document.querySelector("#app > div > div > div.flex-h-box > div.roam-main > div.roam-body-main > div > div > div")
+    if (DNP == false) {
+        roamBody = document.querySelector("#app > div > div > div.flex-h-box > div.roam-main > div.roam-body-main > div > div > div");
+    } else {
+        roamBody = document.querySelector("#rm-log-container > div");
+    }
     roamBody.parentNode.insertBefore(bannerDiv, roamBody);
 }
 
@@ -121,40 +139,53 @@ async function setBannerClip({ extensionAPI }) {
         if (extensionAPI.settings.get("bh-gradient") == true) {
             bannerGradient = true;
         }
-    }
-    const clipText = await navigator.clipboard.readText();
-    if (!isUrl(clipText)) {
-        alert('Please make sure that the clipboard contains a url to an image');
-    } else {
-        var bannerText = "banner: " + clipText;
-        var startBlock = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
-        let q = `[:find (pull ?page [:node/title :block/string :block/uid {:block/children ...} ]) :where [?page :block/uid "${startBlock}"]  ]`;
-        var info = await window.roamAlphaAPI.q(q);
-        if (info.length > 0) {
-            var bannerExists = false;
-            if (info[0][0].hasOwnProperty('children')) {
-                for (var i = 0; i < info[0][0]?.children.length; i++) {
-                    if (info[0][0].children[i].string.match("banner: ")) { // there's already a banner
-                        console.log("Updating page banner");
-                        await window.roamAlphaAPI.updateBlock(
-                            { block: { uid: info[0][0].children[i].uid, string: bannerText.toString(), open: true } });
-                        bannerExists = true;
-                    }
+        const clipText = await navigator.clipboard.readText();
+        if (!isUrl(clipText)) {
+            alert('Please make sure that the clipboard contains a url to an image');
+        } else {
+            var bannerText = "banner: " + clipText;
+            var startBlock = await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
+            var DNP = false;
+            if (!startBlock) {
+                var uri = window.location.href;
+                const regex = /^https:\/\/roamresearch.com\/#\/(app|offline)\/\w+$/; //today's DNP
+                if (uri.match(regex)) { // this is Daily Notes for today
+                    var today = new Date();
+                    var dd = String(today.getDate()).padStart(2, '0');
+                    var mm = String(today.getMonth() + 1).padStart(2, '0');
+                    var yyyy = today.getFullYear();
+                    startBlock = mm + '-' + dd + '-' + yyyy;
+                    DNP = true;
                 }
             }
-            if (bannerExists == false) { // no banners found, create one from clipboard
-                console.log("Creating page banner");
-                var thisBlock = window.roamAlphaAPI.util.generateUID();
-                await window.roamAlphaAPI.createBlock({
-                    location: { "parent-uid": info[0][0].uid, order: 10000 },
-                    block: { string: bannerText.toString(), uid: thisBlock }
-                });
+            let q = `[:find (pull ?page [:node/title :block/string :block/uid {:block/children ...} ]) :where [?page :block/uid "${startBlock}"]  ]`;
+            var info = await window.roamAlphaAPI.q(q);
+            if (info.length > 0) {
+                var bannerExists = false;
+                if (info[0][0].hasOwnProperty('children')) {
+                    for (var i = 0; i < info[0][0]?.children.length; i++) {
+                        if (info[0][0].children[i].string.match("banner: ")) { // there's already a banner
+                            console.log("Updating page banner");
+                            await window.roamAlphaAPI.updateBlock(
+                                { block: { uid: info[0][0].children[i].uid, string: bannerText.toString(), open: true } });
+                            bannerExists = true;
+                        }
+                    }
+                }
+                if (bannerExists == false) { // no banners found, create one from clipboard
+                    console.log("Creating page banner");
+                    var thisBlock = window.roamAlphaAPI.util.generateUID();
+                    await window.roamAlphaAPI.createBlock({
+                        location: { "parent-uid": info[0][0].uid, order: 10000 },
+                        block: { string: bannerText.toString(), uid: thisBlock }
+                    });
+                }
             }
+            if (document.querySelector("div.bannerDiv")) {
+                document.querySelector("div.bannerDiv").remove();
+            }
+            setBanner(clipText, bannerHeight, bannerGradient);
         }
-        if (document.querySelector("div.bannerDiv")) {
-            document.querySelector("div.bannerDiv").remove();
-        }
-        setBanner(clipText, bannerHeight, bannerGradient);
     }
 }
 
